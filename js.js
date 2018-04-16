8505cb4114b0fd9869c685d78923093f93bac8a7
SurikovaElena <36436278+SurikovaElena@users.noreply.github.com>
2018-04-16 12:45:58 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+var imagen1 = "350";
+	var imagen2 = "150";
+function f3(im) {
+	
+	if(im.height == imagen1) im.height = imagen2;
+  else im.height = imagen1;
+}
+but1.onclick = function() {
+	alert ("Заплатите 5000 рублей");
+}
